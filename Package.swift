7eaa1fdd678d18e21fd79db2a99da05c0a59ac0b
woxtu <woxtup@gmail.com>
2018-03-15 02:10:52 +0900

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "RouteKit",
+    products: [
+        .library(
+            name: "RouteKit",
+            targets: ["RouteKit"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "RouteKit",
+            dependencies: []),
+        .testTarget(
+            name: "RouteKitTests",
+            dependencies: ["RouteKit"]),
+    ],
+    swiftLanguageVersions: [4]
+)
